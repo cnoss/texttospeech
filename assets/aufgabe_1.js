31a84f8c53e9390591ee0f2db30a284371f7e130
Alexander <alex-strutz@t-online.de>
2020-05-22 15:21:52 +0200

    First solution

--- assets/aufgabe_1.js
+++ assets/aufgabe_1.js
@@ -1,3 +1,59 @@
-$( document ).ready(function() {
-    //TODO: Your code goes here!
+let synth;
+let utt;
+$(window).on("load", function () {
+
+    synth = window.speechSynthesis;
+    let voiceSelect = $('#voiceSelect');
+
+    let voices = synth.getVoices();
+
+    for (let i in voices) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+        if (voices[i].default) {
+            option.textContent += ' -- DEFAULT';
+        }
+
+        option.value = i;
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        voiceSelect.append(option);
+    }
+
 });
+
+function play() {
+    $('#play-button').hide();
+    $('#pause-button').show();
+    if (synth.paused) {
+        console.log("e")
+        synth.resume();
+    } else {
+        utt = new SpeechSynthesisUtterance(getTexts());
+        utt.pitch = parseFloat($('#speed').val());
+        utt.voice = synth.getVoices()[$('#voiceSelect').val()];
+        console.log($('#volume').val())
+        utt.volume = parseFloat($('#volume').val());
+        synth.speak(utt);
+    }
+
+}
+
+function pause() {
+    $('#pause-button').hide();
+    synth.pause();
+    $('#play-button').show();
+}
+
+function stop() {
+    $('#pause-button').hide();
+    synth.cancel();
+    $('#play-button').show();
+}
+
+function getTexts() {
+    let text = $('.tts-content').find('p').text();
+    return text;
+}
+
